09ba54066483c67627401a4e3184d0e2ad76ca6c
FranciscoEscalante <38298498+FranciscoEscalante@users.noreply.github.com>
2018-04-11 17:52:50 -0300

    dia 1 teoria

---
+++ Practica1/ejercicio1.js
@@ -0,0 +1,8 @@
+//crear dos variables llamadas encendido que sea verdadera y otra llamada horario que valga 9
+//preguntar si se encuentra encendido y el horario es 12, que muestre la lamparita esta encendida fuera de su horario
+//preguntar si se encuentra encendido y el horario es 9, la lamparita funciona correctamente en su horario
+//preguntar si la lamparita se encuentra apagada a las 9, la lamparita no funciona en su horario
+//y sino, como resultado final obligado, mostrar el mensaje el sistema de iluminacion no funciona en todo el dia
+
+//teoria a utilizar: creacion de variables, comparaciones, if, else if, else y console.log para mostrar los mensajes
+
---
+++ Practica1/teoria.js
@@ -0,0 +1,85 @@
+//variables
+// si = true | no = false
+// true false (booleanos = encendido apagado = 1 0)
+// 'A' (char)
+// 'Agustin' (string = cadena de char)
+// 10 50 900 (int = entero)
+// 10.45 (double = doble)
+// 10.45 (float = flotante)
+// 10.45451254521 (float = flotante)
+
+// Letras = ''
+// Numeros = sin ''
+
+//formas de comparacion
+
+// true === false | el resultado es false | le pregunta si la variable o el valor de la izquierda es igual a el de la derecha
+//todo resultado de una comparacion es un booleano, es decir, si es verdadero o falso
+// true !== false | el resultado es true
+
+// && que obliga a que dos resultados sean iguales para que sea true el resultado final
+// || que al menos uno debe ser true
+
+// comentarios -> la computadora no lo lee, es para que el programador o nosotros podamos leer pelotudeces (se suele usar para saber que hace una parte del codigo)
+
+//let delante es para CREAR una variable
+//una variable es una referencia, un nombre al que nosotros le asignamos un valor
+
+//declaramos la variable encendido con el valor true (verdadero, esta encendido)
+// camel case que es una forma de escribir sin espacios. La primer palabra va con minuscula y las demas en vez de espacios, la primer letra es mayuscula.
+
+/*
+let tengoSueno = true
+let horaDeDormir = 22
+
+if (tengoSueno === true && horaDeDormir === 21) {
+    console.log('Me tiro a dormir en la cama')
+}
+
+if (tengoSueno === false || horaDeDormir === 22) {
+    console.log('Me tiro a dormir en la cama')
+}
+*/
+
+// == preguntamos si el de la izquierda es igual a la derecha
+// === preguntamos si el de la izquierda es igual y su tipo de dato tambien es igual
+
+
+
+/*
+if (9 == '8') { //falso, paso al siguiente
+    console.log('valen lo mismo')
+} else if (9 === '9') { //falso, paso al siguiente
+    console.log('valen lo mismo 2')
+} else { //no me queda otra que hacer esto
+    console.log('no valen una mierda')
+}
+*/
+
+// menor < mayor
+// mayor > menor
+// menor <= mayor
+// mayor >= menor
+
+/*
+if (9 < 8) { //falso, paso al siguiente
+    console.log('valen lo mismo')
+} else if (9 <= 9) { //falso, paso al siguiente
+    console.log('valen lo mismo 2')
+} else { //no me queda otra que hacer esto
+    console.log('no valen una mierda')
+}
+*/
+
+/*
+
+if (condicion) {
+    //lo que quiero ejecutar si se cumple
+} else if (condicion) {
+    //otra cosa que quiero ejecutar si se cumple
+} else {
+    //y se ejecuta si o si, si el if no se ejecutó
+}
+
+*/
+
